06cc606bb23d8f50973c05c125a7ea08461cfa24
Natt - <nordonez376@gmail.com>
2025-05-09 17:06:48 -0500

    taller casa

--- Taller-Casa-Perfil/script.js
+++ Taller-Casa-Perfil/script.js
@@ -12,22 +12,20 @@
     localStorage.setItem("perfil", JSON.stringify(perfil))
 
     // Recuperar el objeto desde localStorage
-    const perfilRecuperado = JSON.parse(localStorage.getItem("perfil")) // Recuperar el objeto desde localStorage y convertirlo de nuevo a un objeto
+    const perfilGuardado = JSON.parse(localStorage.getItem("perfil")) // Recuperar el objeto desde localStorage y convertirlo de nuevo a un objeto
  
     // Mostrar los datos recuperados en la consola
-    console.log(perfilRecuperado)
+    console.log(perfilGuardado)
 
     // Mostrar los datos en el div con id="perfil"
     const perfilDiv = document.getElementById("perfil")
     perfilDiv.innerHTML = `
-        <h1>${perfilRecuperado.nombre}</h1>
-        <img src="${perfilRecuperado.foto}" alt="Foto de ${perfilRecuperado.nombre}">
-        <p>Edad: ${perfilRecuperado.edad}</p>
-        <p>Email: ${perfilRecuperado.email}</p>
+        <h1>${perfilGuardado.nombre}</h1>
+        <img src="${perfilGuardado.foto}" alt="Foto de ${perfilGuardado.nombre}">
+        <p>Edad: ${perfilGuardado.edad}</p>
+        <p>Email: ${perfilGuardado.email}</p>
         <h2>Hobbies:</h2>
         <ul>
-            ${perfilRecuperado.hobbies.map(hobby => `<li> ✅ ${hobby}</li>`
-
-            )}
+            ${perfilGuardado.hobbies.map(hobby => `<li>${hobby}</li>`).join('')}
         </ul>
     `
