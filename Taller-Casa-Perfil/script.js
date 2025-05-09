1319bfa73415399bc12a9a8b6c6e361586f8f32c
Natt - <nordonez376@gmail.com>
2025-05-09 16:49:45 -0500

    fin taller casa

--- Taller-Casa-Perfil/script.js
+++ Taller-Casa-Perfil/script.js
@@ -5,13 +5,29 @@
       edad: 23,
       email: "juanperez@gmail.com",
       foto: "https://randomuser.me/api/portraits/men/3.jpg",
-      hobbies: ["Leer", "Programar", "Ir a cine"]
+      hobbies: ["Jugar Fortnite", "Programar", "Estar con mi michi"]
     };
 
     // Almacenar el objeto en localStorage como cadena JSON
+    localStorage.setItem("perfil", JSON.stringify(perfil))
 
     // Recuperar el objeto desde localStorage
+    const perfilRecuperado = JSON.parse(localStorage.getItem("perfil")) // Recuperar el objeto desde localStorage y convertirlo de nuevo a un objeto
  
     // Mostrar los datos recuperados en la consola
+    console.log(perfilRecuperado)
 
     // Mostrar los datos en el div con id="perfil"
+    const perfilDiv = document.getElementById("perfil")
+    perfilDiv.innerHTML = `
+        <h1>${perfilRecuperado.nombre}</h1>
+        <img src="${perfilRecuperado.foto}" alt="Foto de ${perfilRecuperado.nombre}">
+        <p>Edad: ${perfilRecuperado.edad}</p>
+        <p>Email: ${perfilRecuperado.email}</p>
+        <h2>Hobbies:</h2>
+        <ul>
+            ${perfilRecuperado.hobbies.map(hobby => `<li> ✅ ${hobby}</li>`
+
+            )}
+        </ul>
+    `
